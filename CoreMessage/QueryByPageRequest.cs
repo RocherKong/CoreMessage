e8d3f83a36fd4b11d4d8eeb19cd465cad1098644
rocher <rocher@126.com>
2019-07-05 14:11:04 +0800

    add OffSet and QueryRequest

--- CoreMessage/QueryByPageRequest.cs
+++ CoreMessage/QueryByPageRequest.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Core.Message
 {
     public class QueryByPageRequest : RequestMessage
     {
-        [Range(1, 9999999999999)]
-        public int PageIndex { get; set; } = 1;
-        [Range(1, 9999999999999)]
-        public int PageSize { get; set; } = 10;
+        [Range(1, 9999999999999)] public int PageIndex { get; set; } = 1;
+        [Range(1, 9999999999999)] public int PageSize { get; set; } = 10;
+
+        public int Offset => (this.PageIndex - 1) * this.PageSize;
     }
 }
---
+++ CoreMessage/QueryRequest.cs
@@ -0,0 +1,7 @@
+namespace Core.Message
+{
+    public class QueryRequest:RequestMessage
+    {
+        
+    }
+}
